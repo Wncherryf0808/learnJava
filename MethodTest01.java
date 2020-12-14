7fd4fc8ae8ffc7b3061d310beafb251b84f2e6bf
wanghp <1806669257@qq.com>
2020-12-14 12:08:58 +0800

    add sumInt file

---
+++ MethodTest01.java
@@ -0,0 +1,15 @@
+/*
+	方法体当中不能再定义方法
+*/
+
+public class MethodTest01
+{
+	public static void main(String[] args){
+		
+		
+	}
+	public static void sumInt(int a, int b){
+		int c = a + b;
+		System.out.println(a + "+" b + "=" + c)
+	}
+}
